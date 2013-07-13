cb1b8a86f59b5766681507cf4435fb0f89fcc229
Riaz Rahim <riazrahim@gmail.com>
2013-07-13 00:09:58 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html','encoding'));
+  response.send(fs.readFileSync('index.html',encoding));
 });
 
 var port = process.env.PORT || 5000;
